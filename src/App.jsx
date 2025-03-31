446651b11d1833be81b174e56a95a09d1f58c891
mihirbramhane <110406625+mihirbramhane@users.noreply.github.com>
2025-04-01 01:06:55 +0530

    Project files added succesfully
    i

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import "./index.css";
-import Portfolio from "./components/Portfolio";  // Portfolio Component Import Kiya
-
-function App() {
-  return <Portfolio />;  // Direct Portfolio Render Kar Raha Hai
-}
-
-export default App;
-
